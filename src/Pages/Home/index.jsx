1f930255b0b4af2cdbf4b9cbcea87196881e26d3
Thaís Gonçalves <thaisefag1992@gmail.com>
2024-04-26 17:14:40 -0300

    map cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,25 +1,36 @@
 import { useState } from 'react'
 import CardLocaisExercicios from "../../Components/CardLocaisExercicios"
 import listUsuarios from "../../../Data/db-user.json"
+import listLocais from "../../../Data/db-locais.json"
 
 
 
 function home() {
 
-    const stringUser = JSON.stringify(listUsuarios);
-    console.log(stringUser)
+    const stringLocais = listLocais;
+    console.log(stringLocais)
 
-    const listUser = stringUser.map((itemAtual) => {
+    
 
-    })
+    // const listLocaisDt = stringLocais.map((itemAtual) => {
+
+    // })
 
   return (
     <>
-    {listUsuarios.map((itemAtual) => {
-
-    })}
-    </>
+    {stringLocais.map((itemAtual, index) => (
+      <div key={index}>
+        <CardLocaisExercicios nomeLocal={itemAtual.nome_local}
+                              idUsuario={itemAtual.identificador_usuario}
+                              descricaoLocal={itemAtual.descricao_local}
+                              localEnder={itemAtual.loalizacao_endereco}
+                              localCoorden={itemAtual.localizacao_coordenadas}
+                              atividadesExport={itemAtual.tipos_esportes}/>
+      </div>                         
+    ))}
+   
+  </>
   )
 }
 
-export default App
+export default home;
